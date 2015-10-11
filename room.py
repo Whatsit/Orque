ca036fc5c59bca219edb4faa6930b27f456e0e4a
Whatsit <ryanhotte@gmail.com>
2015-10-11 16:56:25 -0700

    Update room.py

--- room.py
+++ room.py
@@ -3,3 +3,10 @@ class Room:
 		self.itemList = []
 		self.playerList = []
 		self.adjacencyList = [0,0,0,0]
+	def checkDoor (self):
+	''' Returns the adjacent room’s direction if there is a locked door (i.e. a puzzle room) adjacent to this room, or None if no such connection exists.'''
+        for x in self.adjacencyList:
+            if x is 2:    # if we find a locked door, return direction of door.
+                return self.adjacencyList.index(x)
+            else
+                return None
