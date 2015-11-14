674a841c879efbfa57cb003c2f2d1e0d3b09639e
Whatsit <ryanhotte@gmail.com>
2015-11-14 12:39:38 -0800

    ui comments

--- ui.py
+++ ui.py
@@ -1,14 +1,17 @@
+"""ui"""
 from tkinter import *
 from player import Player
 import config
 
+"""UI class"""
 class UI:
+	"""Initialize UI"""
 	def __init__(self):
-	
+
 		root = Tk()
 		#root.resizable(width=FALSE, height=FALSE)
 		#root.geometry('{}x{}'.format(200, 100))
-		
+
 		self.output = StringVar()
 
 		frame = Frame(root)
@@ -16,19 +19,21 @@ class UI:
 
 		self.button = Button(frame, text="Quit", fg="red", command=frame.quit)
 		self.button.pack(side = LEFT)
-		
+
 		self.form = Entry(frame)
 		self.form.pack(side = LEFT)
-		
+
 		self.entryButton = Button(frame, text="Enter", fg="green", command=self.setCommand)
 		self.entryButton.pack(side=LEFT)
-		
+
 		self.form2 = Entry(frame, text = self.output, width = 100)
 		self.form2.pack(side = LEFT)
-		
+
 		root.mainloop()
 		root.destroy()
-
+	"""setCommand() Peramiters: None
+	sets command of player = to what user types in ui form.
+	"""
 	def setCommand(self):
 		config.pL[0].command = self.form.get()
 		outputString = config.pL[0].parseCommand()
