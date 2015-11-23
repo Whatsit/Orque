c966ff3c3f54e3a662c5b0c3a7e44ace19ff88d4
Whatsit <ryanhotte@gmail.com>
2015-11-22 16:18:48 -0800

    more attack stuff including weapons and amor

--- orque.py
+++ orque.py
@@ -30,10 +30,24 @@ config.map.randomConnectedMap()
 for p in range(0,1):
 	tmpPlayer = Player(p)
 	config.pL.append(tmpPlayer)
+
+"""	Give players weapen and armor """
+for p in config.pL:
+	weapon = Item(2,"Rusty_Knife","weapon")
+	weapon.effects["weapon"] = 2
+	armor = Item(3,"Old_Hardhat","armor")
+	armor.effects["armor"] = 2
+	potion = Item(4,"Green_Liquid_In_A_Jar","potion")
+	potion.effects["health"] = 10
+	p.inventory.append(weapon)
+	p.inventory.append(armor)
+	p.inventory.append(potion)
+
+
 #config.map.layout[0][0].playerList.append(config.pL[0])
 
 """ Spawn key """
-key = Item(1,"key")
+key = Item(1,"key","key")
 config.map.layout[config.pL[0].location[0]][config.pL[0].location[1]].itemList.append(key)
 #key = Item(2,"key")
 #config.map.layout[config.pL[1].location[0]][config.pL[1].location[1]].itemList.append(key)
@@ -52,7 +66,7 @@ while True:
 		config.map.printMap(config.pL[p].playerId)
 		if(len(config.map.layout[config.pL[p].location[0]][config.pL[p].location[1]].playerList) > 1):
 			print("WARNING :: There is another player in the room!!!")
-			
+
 		config.pL[p].command = input("Input command: ")
 		if config.pL[p].command == "exit":
 			sys.exit()
