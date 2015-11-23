774414ca9984a2e318356b4172bb16ff6c18c599
jeremykr <jeremy.krenbrink@gmail.com>
2015-11-22 16:24:43 -0800

    Added text field with scrollbar
    Can now press 'Return' to input command. Tex field displays command history.

--- ui.py
+++ ui.py
@@ -3,7 +3,6 @@
 Handles UI for Orque
 """
 from tkinter import *
-from player import Player
 import config
 
 class UI:
@@ -17,22 +16,29 @@ class UI:
 		#root.resizable(width=FALSE, height=FALSE)
 		#root.geometry('{}x{}'.format(200, 100))
 
-		self.output = StringVar()
+		self.output = ""
 
 		frame = Frame(root)
 		frame.pack()
-
+		# Quit button
 		self.button = Button(frame, text="Quit", fg="red", command=frame.quit)
 		self.button.pack(side = LEFT)
-
+		# Command entry form
 		self.form = Entry(frame)
 		self.form.pack(side = LEFT)
-
+		# Enter button
 		self.entryButton = Button(frame, text="Enter", fg="green", command=self.setCommand)
 		self.entryButton.pack(side=LEFT)
-
-		self.form2 = Entry(frame, text = self.output, width = 100)
-		self.form2.pack(side = LEFT)
+		# Text field and scroll bar
+		self.scrollbar = Scrollbar(root)
+		self.textField = Listbox(root, height=20, width=50)
+		self.scrollbar.pack(side=RIGHT, fill=Y)
+		self.textField.pack(side=LEFT, fill=Y)
+		self.scrollbar.config(command=self.textField.yview)
+		self.textField.config(yscrollcommand=self.scrollbar.set)
+		self.textField.insert(END, self.output)
+		# Key bindings
+		root.bind("<Return>", self.ReturnPressed)
 
 		root.mainloop()
 		root.destroy()
@@ -43,5 +49,11 @@ class UI:
 		sets command of player = to what user types in ui form.
 		"""
 		config.pL[0].command = self.form.get()
+		self.textField.insert(0, config.pL[0].command)
 		outputString = config.pL[0].parseCommand()
-		self.output.set(outputString)
+		self.textField.insert(0, outputString)
+
+	def ReturnPressed(self, event):
+		self.setCommand()
+		# Clear text from entry widget
+		self.form.delete(0, "end")
